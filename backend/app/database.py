09ef7396affee07bf7150efe9ce01e34766cfe3a
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-08 22:50:16 +0500

    Написал асинхронный движок

---
+++ backend/app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+
+class DataBaseHelper:
+    def __init__(
+            self,
+            url: str,
+            echo: bool = False,
+            echo_pool: bool = False,
+            pool_size: int = 5,
+            max_overflow: int = 10,
+    ):
+
+        self.engine = create_async_engine(
+            url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
