c1f237e060d82e3ad31c2b1e5ae8b6c435618aa0
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-08 23:14:04 +0500

    Написал базовые конфигурации

---
+++ backend/app/config.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class DataBaseConfig(BaseModel): 
+    url: PostgresDsn
+    echo: bool = False
+    echo_bool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
+class RedisConfig(BaseModel):
+    pass
+
+
+class MinioConfig(BaseModel):
+    pass
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="BACK_CONFIG__"
+    )
+
+    run: RunConfig = RunConfig()
+    db: DataBaseConfig
+
+
+settings = Settings()
