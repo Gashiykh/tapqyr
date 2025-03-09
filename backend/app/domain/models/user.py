e941fcbcb4f1c3301c8ad14e675617bbb23f9e1a
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-09 14:47:38 +0500

    Обновил модель Пользователь

--- backend/app/domain/models/user.py
+++ backend/app/domain/models/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Enum, String
+from sqlalchemy import Enum, String, text
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
@@ -18,8 +18,8 @@ class User(Base):
     avatar: Mapped[str] = mapped_column(nullable=True)
 
     tg_username: Mapped[str] = mapped_column(String(32), nullable=False, unique=True)
-    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(nullable=False, server_default=text("false"))
 
-    role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow, null=False)
+    role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), nullable=False, server_default="USER")
+    created_at: Mapped[datetime] = mapped_column(nullable=False, server_default=text("NOW()"))
+    updated_at: Mapped[datetime] = mapped_column(nullable=False, server_default=text("NOW()"), onupdate=datetime.utcnow)
