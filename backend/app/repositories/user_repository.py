51f69777700e8dc44949406d090a50c5daa89ddc
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-09 16:14:12 +0500

    Написал репозитории для Пользователя

---
+++ backend/app/repositories/user_repository.py
@@ -0,0 +1,36 @@
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.domain.models import User
+from app.domain.schemas import UserCreate
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_user_by_id(self, id: int):
+        query = select(User).where(User.id == id)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_user_by_tg(self, tg_name: str):
+        query = select(User).where(User.tg_username == tg_name)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_user_by_username(self, username: str):
+        query = select(User).where(User.username == username)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def create_user(self, user: UserCreate):
+        user = User(**user.model_dump())
+        self.session.add(user)
+        try:
+            await self.session.commit()
+            await self.session.refresh(user)
+            return user
+        except IntegrityError:
+            await self.session.rollback()
+            raise ValueError("Ошибка: username или tg_username уже заняты")
