950eb0f3a01d6f3084e5302aa495d30ef9ec353d
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-10 22:12:45 +0500

    Реализовал хранение токенов в http only cookies

--- backend/app/api/auth_router.py
+++ backend/app/api/auth_router.py
@@ -2,7 +2,8 @@ from fastapi import (
     Depends,
     APIRouter,
     HTTPException,
-    status
+    status,
+    Response
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -11,14 +12,15 @@ from app.repositories import UserRepository
 from app.services import AuthService
 from app.domain.schemas import (
     UserCreate,
-    TokenResponse, UserLogin
+    UserLogin
 )
 
 router = APIRouter()
 
-@router.post("/register", response_model=TokenResponse)
+@router.post("/register")
 async def register(
         user_data: UserCreate,
+        response: Response,
         session: AsyncSession = Depends(db_helper.session_getter),
 ):
     user_repo = UserRepository(session)
@@ -26,20 +28,34 @@ async def register(
 
     try:
         access_token, refresh_token = await auth_service.register_user(user_data)
-        return TokenResponse(
-            access_token=access_token,
-            refresh_token=refresh_token
-        )
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
         )
 
+    response.set_cookie(
+        key='access_token',
+        value=access_token,
+        httponly=True,
+        secure=False, # для localhost False; в продакшене True (HTTPS)
+        samesite="strict"
+    )
 
-@router.post("/login", response_model=TokenResponse)
+    response.set_cookie(
+        key='refresh_token',
+        value=refresh_token,
+        httponly=True,
+        secure=False, # TODO поменять на True
+        samesite="strict"
+    )
+
+    return {"detail": "Пользователь успешно зарегистрирован."}
+
+@router.post("/login")
 async def login(
         user_login: UserLogin,
+        response: Response,
         session: AsyncSession = Depends(db_helper.session_getter),
 ):
     user_repo = UserRepository(session)
@@ -47,12 +63,26 @@ async def login(
 
     try:
         access_token, refresh_token = await auth_service.login(user_login)
-        return TokenResponse(
-            access_token=access_token,
-            refresh_token=refresh_token
-        )
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
         )
+
+    response.set_cookie(
+        key='access_token',
+        value=access_token,
+        httponly=True,
+        secure=False,  # для localhost False; в продакшене True (HTTPS)
+        samesite="strict"
+    )
+
+    response.set_cookie(
+        key='refresh_token',
+        value=refresh_token,
+        httponly=True,
+        secure=False,  # TODO поменять на True
+        samesite="strict"
+    )
+
+    return {"detail": "Пользователь успешно авторизован."}
