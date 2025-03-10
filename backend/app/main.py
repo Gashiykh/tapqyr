eb96cd87c1a226f068fd712bdf754c22ff536d81
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-10 14:53:34 +0500

    Поправил импорты

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from app.api.auth_router import router as auth_router
+from app.api import router as auth_router
 from app.core import settings, db_helper
 
 
