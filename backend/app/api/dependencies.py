48811c51b8901df07c8a2b2f7886985a46b9187a
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-15 19:14:27 +0500

    Добавил зависимость для получения текущего пользователя

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,10 +1,18 @@
-from http.client import responses
-
-from fastapi import Depends, Response
+import jwt
+from fastapi import (
+    Depends,
+    Response,
+    Cookie,
+    HTTPException,
+    status
+)
+from jwt import decode
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing_extensions import Optional
 
-from app.core import db_helper
+from app.core import db_helper, settings
 from app.repositories import UserRepository
+from app.security import decode_token
 from app.services import AuthService
 
 
@@ -55,3 +63,31 @@ def delete_tokens_from_cookies(
         samesite="strict",
         secure=False
     )
+
+
+async def get_current_user(
+        access_token: Optional[str] = Cookie(None),
+        session: AsyncSession = Depends(db_helper.session_getter)
+):
+    if not access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Пользователь не авторизован"
+        )
+    try:
+        user_id, role = decode_token(access_token)
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Токен просрочен")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Невалидный токен")
+
+    user_repo = UserRepository(session)
+    user = await user_repo.get_user_by_id(user_id)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Пользователь не найден"
+        )
+
+    return user
