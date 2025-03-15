b2d609185f6448f176ee0f4e10bd377407f7befe
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-15 12:54:15 +0500

    Удалил схему получения токенов т.к храним токены в cookies

--- backend/app/domain/schemas/auth.py
+++ backend/app/domain/schemas/auth.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel
 
 
-class TokenResponse(BaseModel):
-    token_type: str = "Bearer"
-    access_token: str
-    refresh_token: str
-
-
 class UserLogin(BaseModel):
     username: str
     password: str
