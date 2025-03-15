00c0395d5203aedc6b7debcad5f258c3dd6cfab9
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-15 19:12:05 +0500

    Добавил валидацию для пароля

--- backend/app/domain/schemas/user.py
+++ backend/app/domain/schemas/user.py
@@ -1,5 +1,12 @@
-from datetime import datetime
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    Field,
+    field_validator
+)
+from fastapi import (
+    HTTPException,
+    status
+)
 
 from app.domain.enums import RoleEnum
 
@@ -10,7 +17,28 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(
+        ...,
+        min_length=8,
+        description="Пароль должен содержать минимум 8 символов"
+    )
+    confirm_password: str
+
+    @field_validator("confirm_password")
+    @classmethod
+    def passwords_match(cls, confirm_password, values):
+        password = values.data.get("password")
+        if password and confirm_password != password:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Пароли не совпадают"
+            )
+        return confirm_password
+
+    def model_dump(self, **kwargs):
+        data = super().model_dump(**kwargs)
+        data.pop("confirm_password", None)
+        return data
 
 
 class UserRead(UserBase):
@@ -18,5 +46,3 @@ class UserRead(UserBase):
     avatar: str | None = None
     role: RoleEnum = RoleEnum.USER
     is_verified: bool
-    created_at: datetime
-    updated_at: datetime
