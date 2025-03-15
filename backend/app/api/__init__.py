1ff59a10ba4989b6649c65210c914948cc438f1a
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-15 19:14:40 +0500

    Настроил импорты

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,1 +1,2 @@
-from .auth_router import router
+from .auth_router import router as auth_router
+from .user_router import router as user_router
--- backend/app/security/__init__.py
+++ backend/app/security/__init__.py
@@ -1,5 +1,6 @@
 from .pass_hashing import hash_password, verify_password
 from .jwt_handler import (
     create_access_token,
-    create_refresh_token
+    create_refresh_token,
+    decode_token
 )
